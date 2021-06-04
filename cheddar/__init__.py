821fd7e0b77878e5dd8eb10e3446da57aedd4138
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-06-04 21:03:29 +0530

    Barebones stuff implemented, need to think about creating game workflow and rooms on server side

--- cheddar/__init__.py
+++ cheddar/__init__.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, session
 from cheddar import api
+from chess import Board
 
 def create_app():
-	app = Flask(__name__)
+	app = Flask(__name__, static_url_path='', static_folder='static/')
+	app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 	app.register_blueprint(api.bp)
-
-	@app.route('/')
-	def index():
-		return 'Hello, World!'
 	
 	return app
--- cheddar/api.py
+++ cheddar/api.py
@@ -1,14 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, request, session, g
+import chess
+from chess import Board
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/move')
 def move():
-    return 'Making move'
+    if 'game' not in g:
+        g.game = chess.STARTING_FEN
+    from_sq = request.args.get("from")
+    to_sq = request.args.get("to")
+    print(f"{from_sq} -> {to_sq}")
+    board = Board(fen=g.game)
+    try:
+        legal_move = board.find_move(chess.parse_square(from_sq), chess.parse_square(to_sq))
+        board.push(legal_move)
+        g.game = board.fen()
+        print("Found legal move")
+        print(board)
+        return "LEGAL"
+    except:
+        print("Found illegal move")
+        print(board)
+        return "ILLEGAL"
 
 @bp.route('/join_game')
 def join_game():
     return 'joining game'
+
 
 @bp.route('/board')
 def board():
